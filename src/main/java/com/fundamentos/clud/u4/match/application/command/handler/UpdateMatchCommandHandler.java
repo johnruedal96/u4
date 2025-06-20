91992b58eb4b41c58a7eea06fb52a14b1331d591
John Rueda <johnruedal96@gmail.com>
2025-06-20 08:58:49 -0500

    remove imports

--- src/main/java/com/fundamentos/clud/u4/match/application/command/handler/UpdateMatchCommandHandler.java
+++ src/main/java/com/fundamentos/clud/u4/match/application/command/handler/UpdateMatchCommandHandler.java
@@ -1,11 +1,8 @@
 package com.fundamentos.clud.u4.match.application.command.handler;
 
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.fundamentos.clud.u4.match.application.command.CreateMatchCommand;
 import com.fundamentos.clud.u4.match.application.command.UpdateMatchCommand;
 import com.fundamentos.clud.u4.match.domain.Match;
 import com.fundamentos.clud.u4.match.domain.MatchRepository;
